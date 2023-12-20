f96a73eca6354cdf7178182681f4b23b373cd795
boyi wu <954576326@qq.com>
2023-12-20 13:26:53 +0800

    client init

---
+++ client/src/views/dashboard/index.vue
@@ -0,0 +1,75 @@
+<script>
+import { mapGetters } from 'vuex'
+import adminDashboard from './admin'
+import studentDashboard from './student'
+import RecordTable from './components/RecordTable.vue'
+export default {
+  name: 'Dashboard',
+  components: { adminDashboard, studentDashboard, RecordTable },
+  data() {
+    return {
+      currentRole: 'adminDashboard'
+    }
+  },
+  computed: {
+    ...mapGetters(['roles'])
+  },
+  created() {
+    // if (this.roles.includes('student')) {
+    //   this.currentRole = 'studentDashboard'
+    // }
+  },
+  methods: {}
+}
+</script>
+
+<template>
+  <div class="dashboard-container">
+    <!-- <component :is="currentRole" /> -->
+
+    <div class="wrapper">
+      <!-- <PanelGroup :building-id="buildingId"></PanelGroup> -->
+    </div>
+    <!-- 管理概览 -->
+    <!-- 公共财产管理 -->
+    <h1 class="main-title">小区公共财产</h1>
+    <div class="wrapper">
+      <RecordTable
+        type="public"
+        title="公共财产"
+        icon="el-icon-suitcase"
+        :table-data="[]"
+        :show-pagination="false"
+      ></RecordTable>
+    </div>
+
+    <!-- <h1 class="main-title">最新公告</h1> -->
+    <!-- 最新学生早起情况 -->
+    <!-- <div class="wrapper">
+      <RecordTable
+        type="getup"
+        title="早起动态"
+        icon="el-icon-alarm-clock"
+        :table-data="getupTableData"
+        :show-pagination="false"
+      ></RecordTable>
+    </div> -->
+
+    <!-- 最新学生早起情况 -->
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.dashboard-container {
+  padding: 0px 60px 0px;
+  .main-title {
+    display: flex;
+    align-items: center;
+  }
+  .wrapper {
+    border-radius: 5px;
+    overflow: hidden;
+    margin: 40px 0;
+  }
+}
+</style>
---
+++ client/src/views/property-manage/components/editProperty.vue
@@ -0,0 +1,26 @@
+<script>
+import propertyForm from './propertyForm.vue'
+export default {
+  name: 'editProperty',
+  components: {
+    propertyForm
+  },
+  data() {
+    return {
+      fromData: {}
+    }
+  },
+  methods: {
+    handleSubmit(data) {
+      //  处理修改逻辑
+    },
+    show() {
+      this.visible = true
+    }
+  }
+}
+</script>
+<template>
+  <propertyForm @finish="handleSubmit" :submitText="新增"> </propertyForm>
+</template>
+<style scoped lang="scss"></style>
